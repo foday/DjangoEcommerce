0b258b0567cc78d200bd0e1003711eb3e10ed8fd
foday <foday_s@yahoo.com>
2021-03-28 20:38:46 -0500

    Product Table with added field
    title, author, description, image, slug, price, in_stock, and is_active fields or columns are added with models class and CharFields with parameters of max_length. The TextField, ImageField are also used.

--- store/models.py
+++ store/models.py
@@ -15,4 +15,13 @@ class Product(models.Model):
     category = models.ForeignKey(Category, related_name='product', on_delete=models.CASCADE)
     created_by = models.ForeignKey(User, on_delete=models.CASCADE, related_name='product_creator')
     title = models.CharField(max_length=255)
-    author = models.
+    author = models.CharField(max_length=255, default='admin')
+    description = models.TextField(blank=True)
+    image = models.ImageField(upload_to='images/')
+    slug = models.SlugField(max_length=255)
+    price = models.DecimalField(max_digits=4, decimal_places=2)
+    in_stock = models.BooleanField(default=True)
+    is_active = models.BooleanField(default=True)
+    
+
+
